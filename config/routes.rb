4548456790eb210ae34d01f86f72c77a3feb745d
pohl989 <pohl989@gmail.com>
2018-09-08 11:52:23 -0600

    added generic navbar and footer

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+  root 'bios#index'
   resources :bios
   resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
