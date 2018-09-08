ea277d97fa0a3086f3bd58d26cae258669b0562a
pohl989 <pohl989@gmail.com>
2018-09-08 14:04:54 -0600

    post_view template

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,28 @@
 class User < ApplicationRecord
+  has_many :posts
+  has_many :comments
+  has_one :bio
+
+  after_create :create_bio
+  before_destroy :destroy_bio
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+
+
+  private
+
+
+  def before_create
+    self.build_bio
+  end
+
+  def destroy_bio
+    self.bio.destroy
+  end
+
+
+
 end
